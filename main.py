b705a59e0b1060977845340afa1898d0ff04e519
ebil <you@exaple.com>
2022-04-14 02:48:53 +0100

    Updated config, added try-except block

--- main.py
+++ main.py
@@ -15,38 +15,41 @@ def get_data(market, interval):
     unix_time = int(time.time())
     print(f"Writing data for {market}")
 
-    while True:
-        req = requests.get(f"{BASE_URL}pair={market}&contractType=PERPETUAL&interval={interval}&startTime={START_TIME}")
-        json = req.json()
+    try:
+        while True:
+            req = requests.get(f"{BASE_URL}pair={market}&contractType=PERPETUAL&interval={interval}&startTime={START_TIME}")
+            json = req.json()
 
-        for i in json:
-            string_to_write =f"{i[0]},{i[1]},{i[2]},{i[3]},{i[4]},{i[5]}\n"
-            write_data(market, string_to_write)
+            for i in json:
+                string_to_write =f"{i[0]},{i[1]},{i[2]},{i[3]},{i[4]},{i[5]}\n"
+                write_data(market, string_to_write)
 
-        start_time = i[0]
-        break
-
-    while True:
-        req = requests.get(f"{BASE_URL}pair={market}&contractType=PERPETUAL&interval={interval}&startTime={start_time}")
-        json = req.json()
-
-        k = 0
-        for i in json:
-            string_to_write =f"{i[0]},{i[1]},{i[2]},{i[3]},{i[4]},{i[5]}\n"
-            write_data(market, string_to_write)
-            k += 1
-
-        if k < 150:
-            """
-            If k < 150 (arbitrary number less than 500) we've reached the current time and so can exit the loop.
-            Could use the timestamp returned by the API but the formatting is different
-            the time library and I'm too lazy to fix it
-            """
-            print(k)
+            start_time = i[0]
             break
 
-        start_time = i[0]
-        sleep(1)
+        while True:
+            req = requests.get(f"{BASE_URL}pair={market}&contractType=PERPETUAL&interval={interval}&startTime={start_time}")
+            json = req.json()
+
+            k = 0
+            for i in json:
+                string_to_write =f"{i[0]},{i[1]},{i[2]},{i[3]},{i[4]},{i[5]}\n"
+                write_data(market, string_to_write)
+                k += 1
+
+            if k < 150:
+                # If k < 150 (arbitrary number less than 500) we've reached the current time and so can exit the loop.
+                break
+
+            start_time = i[0]
+            sleep(1)
+
+    except IndexError:
+        print(f"IndexError: Error writing data for {market}")
+
+    except:
+        print(f"Uncaught Error: Error writing data for {market}")
+
 
 def write_data(market, string_to_write):
     directory = f"data/{INTERVAL}/"
